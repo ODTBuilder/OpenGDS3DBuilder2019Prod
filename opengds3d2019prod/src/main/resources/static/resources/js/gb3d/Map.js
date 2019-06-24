c413aa81dd9162d2184955d06483a4c8a426e206
SOYIJUN <monkey1221@git.co.kr>
2019-06-24 09:17:33 +0900

    예시

--- opengds3d2019prod/src/main/resources/static/resources/js/gb3d/Map.js
+++ opengds3d2019prod/src/main/resources/static/resources/js/gb3d/Map.js
@@ -1,3 +1,32 @@
 /**
- * 
+ * @namespace {Object} gb3d
  */
+var gb3d;
+if (!gb3d)
+	gb3d = {};
+/**
+ * @classdesc Map 객체를 정의한다.
+ * 
+ * @class gb3d.Map
+ * @memberof gb3d
+ * @param {Object}
+ *            obj - 생성자 옵션을 담은 객체
+ * @param {HTMLElement}
+ *            obj.target - 지도 영역이 될 Div의 HTMLElement
+ * @author SOYIJUN
+ */
+gb3d.Map = function(obj) {
+
+	var that = this;
+	var options = obj ? obj : {};
+}
+
+/**
+ * 함수 설명
+ * 
+ * @method gb3d.Map#functionname
+ * @return {ol.Map} 리턴 객체
+ */
+gb3d.Map.prototype.get = function() {
+	return this;
+};
